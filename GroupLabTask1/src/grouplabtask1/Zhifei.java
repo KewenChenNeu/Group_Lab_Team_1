ee3ab1a1921a7d3c8d777c082bf8c11009986307
Fibi <zy94@cornell.edu>
2025-10-19 12:27:31 -0700

    Hello from Zhifei & Main.java updated

--- GroupLabTask1/src/grouplabtask1/Zhifei.java
+++ GroupLabTask1/src/grouplabtask1/Zhifei.java
@@ -6,12 +6,10 @@ package grouplabtask1;
 
 /**
  *
- * @author chris
+ * @author Olive
  */
-public class Main {
-    public static void main(String[] args) {
-        Kewen.main(args);
-        Jinkun.main(args);
-        Zhifei.hello();
+public class Zhifei {
+    public static void hello(){
+        System.out.println("Hello from Zhifei");
     }
 }
